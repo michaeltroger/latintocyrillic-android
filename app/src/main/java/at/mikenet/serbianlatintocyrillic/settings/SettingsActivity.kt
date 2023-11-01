67ae769aee4a389f118167f2e616f1d75e94a433
Michael Troger <dev@michaeltroger.com>
2023-11-01 20:00:20 +0100

    Remove deprecated call

--- app/src/main/java/at/mikenet/serbianlatintocyrillic/settings/SettingsActivity.kt
+++ app/src/main/java/at/mikenet/serbianlatintocyrillic/settings/SettingsActivity.kt
@@ -19,7 +19,7 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
-            onBackPressed()
+            onBackPressedDispatcher.onBackPressed()
             return true
         }
         return super.onOptionsItemSelected(item)
