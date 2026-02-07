1ea5bbe8ee7dc8e652f925f4823bfb2465da7967
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2026-02-07 01:30:48 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "at.mikenet.serbianlatintocyrillic"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "at.mikenet.serbianlatintocyrillic"
